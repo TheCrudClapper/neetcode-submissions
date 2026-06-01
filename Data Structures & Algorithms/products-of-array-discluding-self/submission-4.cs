ec9948b96c12f758b9b859acb76901691136b683
Wojciech Mucha <156688078+TheCrudClapper@users.noreply.github.com>
2026-06-01 13:31:47 +0200

    Add: products-of-array-discluding-self - submission-4

---
+++ Data Structures & Algorithms/products-of-array-discluding-self/submission-4.cs
@@ -0,0 +1,70 @@
+public class Solution {
+    public int[] ProductExceptSelf(int[] nums) {
+        //Brute Force O(n^2)
+        // int[] output = new int[nums.Length];
+
+        // for(int i = 0; i < nums.Length; i++)
+        // {
+        //     int product = 1;
+        //     for(int j = 0; j < nums.Length; j++)
+        //     {   
+        //         if(j == i)
+        //             continue;
+
+        //         product = product * nums[j];
+        //     }
+        //     Console.WriteLine(product);
+        //     output[i] = product;
+        // }
+
+        // return output;
+
+        //Optimal O(n) Prefix/Suffix Dedicated Arrays
+        // int[] output = new int[nums.Length];
+        
+        // int[] prefix = new int[nums.Length];
+        // int[] suffix = new int[nums.Length];
+
+        // int prefixNum = 1;
+        // int suffixNum = 1;
+        // for(int i = 0; i < nums.Length; i++)
+        // { 
+        //     prefix[i] = prefixNum;
+        //     Console.WriteLine(prefix[i]);
+        //     prefixNum = prefixNum * nums[i];
+        // }
+
+        // for(int i = nums.Length - 1; i >= 0; i--)
+        // {
+        //     suffix[i] = suffixNum;
+        //     Console.WriteLine(suffix[i]);
+        //     suffixNum = suffixNum * nums[i];
+        // }
+
+
+        // for(int i = 0; i < nums.Length; i++)
+        //     output[i] = prefix[i] * suffix[i];
+        
+        // return output;
+
+        //Recommended O(n)
+        int[] output = new int[nums.Length];
+
+        int prefixNum = 1;
+        for(int i = 0; i < nums.Length; i++)
+        { 
+            output[i] = prefixNum;
+            prefixNum = prefixNum * nums[i];
+        }
+
+        var suffixNum = 1;
+        for(int i = nums.Length - 1; i >= 0; i--)
+        {
+            output[i] = output[i] * suffixNum;
+            suffixNum = suffixNum * nums[i];
+        }
+
+        return output;
+
+    }
+}
