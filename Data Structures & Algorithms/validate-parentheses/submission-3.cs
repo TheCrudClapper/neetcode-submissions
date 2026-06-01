1c672c567f1d81f417eac3f14ace3ef3b9bac68d
Wojciech Mucha <156688078+TheCrudClapper@users.noreply.github.com>
2026-06-01 15:16:10 +0200

    Add: validate-parentheses - submission-3

---
+++ Data Structures & Algorithms/validate-parentheses/submission-3.cs
@@ -0,0 +1,43 @@
+public class Solution {
+    public bool IsValid(string s) {
+        //Brute Force 
+        // while (true)
+        // {
+        //     string previous = s;
+
+        //     s = s.Replace("[]", "");
+        //     s = s.Replace("()", "");
+        //     s = s.Replace("{}", "");
+
+        //     if (s.Length == 0)
+        //         return true;
+
+        //     if (previous == s)
+        //         return false;
+               
+        // }
+
+        //Stack-based approach
+        Stack<char> stack = new();
+        for(int i = 0; i < s.Length; i++)
+        {
+            if(s[i] == '(' || s[i] == '{' || s[i] == '[')
+                stack.Push(s[i]);
+
+            if(s[i] == ')' || s[i] == '}' || s[i] == ']')
+            {
+                if(stack.Count == 0) return false;
+                char top = stack.Pop();
+                if ((s[i] == ')' && top != '(') || 
+                    (s[i] == '}' && top != '{') || 
+                    (s[i] == ']' && top != '['))
+                    return false;
+            }
+        }
+
+        if(stack.Count == 0)
+            return true;
+        else
+            return false;
+    }
+}
