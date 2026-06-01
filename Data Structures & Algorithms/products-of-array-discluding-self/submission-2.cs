af27ce66350a9ac08414b021c17fee64bda4a27a
Wojciech Mucha <156688078+TheCrudClapper@users.noreply.github.com>
2026-06-01 12:51:25 +0200

    Add: products-of-array-discluding-self - submission-2

---
+++ Data Structures & Algorithms/products-of-array-discluding-self/submission-2.cs
@@ -0,0 +1,22 @@
+public class Solution {
+    public int[] ProductExceptSelf(int[] nums) {
+        //Brute Force O(n^2)
+        int[] output = new int[nums.Length];
+
+        for(int i = 0; i < nums.Length; i++)
+        {
+            int product = 1;
+            for(int j = 0; j < nums.Length; j++)
+            {   
+                if(j == i)
+                    continue;
+
+                product = product * nums[j];
+            }
+            Console.WriteLine(product);
+            output[i] = product;
+        }
+
+        return output;
+    }
+}
